bc15274c0836f37148098545e85ad391019fb2d1
Harsh Yadav <hy31853185@gmail.com>
2026-01-28 01:02:58 +0530

    message

---
+++ 01_basics/comparision.js
@@ -0,0 +1,17 @@
+
+/* Try to avoid these type of conversions */
+
+ // console.log("2" > 1);
+
+
+console.log( null > 0 );
+console.log(null ==  0);
+console.log(null >= 0 );
+
+console.log(undefined == 0);
+console.log(undefined > 0 );
+console.log(undefined < 0 );
+
+
+
+console.log("2" === 2);
