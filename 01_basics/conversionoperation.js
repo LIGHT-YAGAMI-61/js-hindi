a6c2984cd84a1495413a3eff34cb249ea4c5006b
Harsh Yadav <hy31853185@gmail.com>
2026-01-27 18:12:08 +0530

    Organised JS files into 01_basics folder

--- 01_basics/conversionoperation.js
+++ 01_basics/conversionoperation.js
@@ -24,6 +24,31 @@ let score = "33ab" ;
  let someNumber = 33 ; 
 
  let stringNumber = String(someNumber) ;
- console.log(stringNumber);
- console.log( typeof stringNumber);
+  // console.log(stringNumber);
+ // console.log( typeof stringNumber);
+ 
+
+//  ********************* Operations **********************
+
+let value = 3 ;
+let negValue = -value ; 
+ // console.log(negValue);
+
+//  console.log(2 + 2);
+
+let str1 = "hello" ;
+let str2 = " harsh" ;
+let str3 =  str1 + str2 ; 
+console.log(str3);
+
+ // console.log( "1" + 2 );
+// console.log("1" + 2 + 2);
+ // console.log(1 + 2 + "3");
+
+console.log(+true);
+
+let gamecounter = 100 ;
+gamecounter++ ;
+console.log(gamecounter);
+
  
