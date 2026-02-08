84dd87dfa7210efa2f019ae3413def89eb4642fa
Harsh Yadav <hy31853185@gmail.com>
2026-02-08 22:46:48 +0530

    More about functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -22,15 +22,15 @@
 
      function addTwoNumbers(number1 , number2) { // <- parameters
    
-      //  let result = number1 + number2 
+        let result = number1 + number2 
        // return result 
-       // console.log("kai")
+      //  console.log("kai")
        return number1 + number2
         
   }
 
   const result  = addTwoNumbers(3 , 5) 
- // console.log("Result : ", result) ; 
+   // console.log("Result : ", result) ; 
    
     function loginUserMessage(username = "eren") {
         if ( !username) {
@@ -40,6 +40,38 @@
         return `${username} just logged in`
     }
 
+    // *************************************************************
+    
    // console.log(loginUserMessage("kai"));
     console.log(loginUserMessage());
     
+    function calculateCartPrice(val1 , val2 , ...num1) {
+        return num1 
+    }
+
+    // console.log(calculateCartPrice(200 , 400 , 500 , 2000 ));
+    
+    const user = {
+        username : "Harsh" ,
+        price : 199
+    }
+    function handleObject(anyobject) {
+       // console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+        
+    }
+    // handleObject(user) ;
+    handleObject( {
+        username : "kai" ,
+        price : 399
+    })
+
+    const myNewArray = [200 , 400 , 100 , 600]
+
+    function returnSecondValue(getArray) {
+        return getArray[1] 
+    }
+
+   //  console.log(returnSecondValue(myNewArray));
+    
+   console.log(returnSecondValue([200 , 400 , 500 , 1000]));
+   
