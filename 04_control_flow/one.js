997f3d7fe78713b2920f00d2d089e4961793955f
Harsh Yadav <hy31853185@gmail.com>
2026-02-13 00:20:23 +0530

    About if else conditions

---
+++ 04_control_flow/one.js
@@ -0,0 +1,53 @@
+
+// if 
+
+    const isUserloggedIn = true 
+
+    const temperature = 41 
+    // if ( temperature === 41  ) {
+    //     console.log("less then 50");
+        
+    // } else {
+    // console.log("temperature is greater then 50 ");
+    // }
+
+   // console.log("Executed");
+    
+   // < ,  > , <= , >= , == , != , === , !===
+
+   const score = 200 
+
+   if ( score > 100 ) {
+    let power = "fly" 
+    // console.log(`User power : ${power}`);
+    
+   }
+   // console.log(`User power : ${power}`);
+
+   // Short hand notation
+
+   const balance = 1000 
+//    if ( balance > 500 ) console.log("test") , console.log("test2");
+   
+    // if ( balance < 500 ) {
+    //     console.log("less than 500");
+    // }  else if ( balance < 750 ) {
+    //     console.log("less then 750");
+    // }  else if ( balance < 900 ) {
+    // }  else {
+    //     console.log("less then 1200 ");
+    // }
+   
+    const isUserLoggedIn = true 
+    const debitCard = true 
+    const loggedInFromGoogle = false 
+    const loggedInFromEmail = true
+
+    if ( isUserLoggedIn && debitCard  && 2 == 2 ) {
+        console.log("Allow to buy courses");
+    }
+
+    if ( loggedInFromGoogle || loggedInFromEmail ) {
+        console.log("User Logged in ");
+        
+    }
