c3aa23bbcadfb083a61608b39e529e1fabf6a726
Harsh Yadav <hy31853185@gmail.com>
2026-02-17 00:23:28 +0530

    About Maps

---
+++ 05_iterations/three.js
@@ -0,0 +1,40 @@
+
+// for of 
+
+    // ["" , "", ""] 
+    // [{} , {}, {}]
+
+    const arr = [ 1 , 2 , 3 ,4 , 5] 
+
+    for (const num of arr ) {
+       // console.log(num);
+    }
+
+    const greetings = "Hello world!" 
+    for (const greet of greetings ) {
+       // console.log(`each char is ${greet}`);
+    }
+
+    // MAPS  ( Unique values )
+
+    const map = new Map() 
+    map.set('IN', "India")
+    map.set('USA', "United Sates of America")
+    map.set('Fr' , " France ")
+    map.set('IN', "India")
+
+    // console.log(map);
+    
+    for (const [key , value] of map) {
+        console.log(key , ':-', value);
+    }
+
+    const myObject = {
+        'game1' : 'NFS' , 
+        'game2' : 'spiderman'
+    }
+
+    // for (const [key , value] of myObject) {
+    //     console.log(key , ':-' , value);
+        
+    // }
