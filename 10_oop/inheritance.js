e1ca2ce19a85d57d4bb853cb610aa5a3490a3e77
Harsh Yadav <hy31853185@gmail.com>
2026-03-13 19:59:45 +0530

    About Inheritance in JS

---
+++ 10_oop/inheritance.js
@@ -0,0 +1,33 @@
+class User {
+    constructor(username) {
+    this.username = username 
+}
+logMe() {
+    console.log(`USERNAME IS ${this.username}`);
+    
+   }
+}
+
+class Teacher extends User {
+    constructor(username , email , password) {
+        super(username)
+        this.email = email 
+        this.password = password 
+    }
+
+    addCourse() {
+        console.log(`A new course was added by ${this.username}`);
+        
+    }
+}
+
+const harsh = new Teacher("kai" , "kai@gmail.com" , "567")
+
+harsh.addCourse()
+const hk = new User("xxx")
+
+hk.logMe()
+
+console.log(harsh instanceof User);
+        // or
+// console.log(harsh === hk);
